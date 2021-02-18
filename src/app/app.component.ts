4cbef727bb9de14a21bddcc329f99004089234e8
Caleb Pomar <cpsharksfan@gmail.com>
2021-02-18 11:36:17 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+//////////////////////
+// Needed URLS
+/////////////////////
+
+const QUERY_URL = 'http://localhost:3000/sky/cloud/ckl9qtx1h000n48vlfkmw2u70';
+const EVENT_URL = 'http://localhost:3000/sky/cloud/event/ckl9qtx1h000n48vlfkmw2u70/client';
+
+//////////////////////////
+// Interfaces
+//////////////////////////
+
+interface Profile {
+  name?: string,
+  location?: string,
+  threshold?: number,
+  sms_number?: string
+}
+
+interface Reading {
+  temperature: number,
+  timestamp: string | Date,
+}
+
+//////////////////////////
+// Component Definition
+//////////////////////////
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lab5client';
+export class AppComponent implements OnInit{
+  currentReading: Reading;
+  readings: Reading[] = [];
+  threshold: number = 65;
+  showStart = true;
+  profile: Profile;
+  editing = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.refresh();
+    this.setProfile();
+  }
+
+  /////////////////////////
+  // API Calls
+  /////////////////////////
+
+  getTemperatureReadings() {
+    return this.http.get(`${QUERY_URL}/temperature_store/temperatures`)
+  }
+
+  getThresholdViolations() {
+    return this.http.get(`${QUERY_URL}/temperature_store/threshold_violations`)
+  }
+
+  getProfile() {
+    return this.http.get(`${QUERY_URL}/sensor_profile/profile`)
+  }
+
+  updateProfile(profile: Profile) {
+    return this.http.post(`${EVENT_URL}/sensor/profile_updated`, profile)
+  }
+
+  /////////////////////////
+  // UI Actions
+  ////////////////////////
+
+  refresh() {
+    this.getTemperatureReadings().subscribe((r: Reading[]) => {
+      this.readings = r.map(el => {
+        el.timestamp = new Date(el.timestamp);
+        return el;
+      });
+      this.currentReading = this.readings[0];
+      console.log(r);
+    });
+  }
+
+  setProfile() {
+    this.getProfile().subscribe((p: Profile) => this.profile = p)
+  }
+
+  togglePage() {
+    this.showStart = !this.showStart;
+  }
+
+  saveChanges() {
+    this.updateProfile(this.profile).subscribe();
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
